b32161c7131538995fc765e314331b4365578fa4
Noah Allen <noahtallen@gmail.com>
2020-01-24 15:24:25 -0800

    Start working on e2e test config

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/tests/e2e/fse-back-button.spec.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/tests/e2e/fse-back-button.spec.js
@@ -2,14 +2,14 @@
  * External dependencies
  */
 /* eslint-disable import/no-extraneous-dependencies */
-// import { createNewPost } from '@wordpress/e2e-test-utils';
+import { createNewPost } from '@wordpress/e2e-test-utils';
 
 describe( 'Full Site Editing Back Button', () => {
-	// beforeAll( async () => {
-	// 	await await createNewPost( {
-	// 		postType: 'page',
-	// 	} );
-	// } );
+	beforeAll( async () => {
+		await createNewPost( {
+			postType: 'page',
+		} );
+	} );
 
 	it( 'Should be true.', async () => {
 		await expect( true ).toBe( true );
