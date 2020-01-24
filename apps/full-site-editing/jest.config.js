591c97963cf08ee0e9d721f5a8ea584514debbe2
Noah Allen <noahtallen@gmail.com>
2020-01-24 13:55:57 -0800

    Fix local js test config
    - Needed an update to support running without local node_modules
    - Updated to match .test and .spec files in the FSE plugin
    - Move setup file to `bin`

--- apps/full-site-editing/jest.config.js
+++ apps/full-site-editing/jest.config.js
@@ -1,11 +1,27 @@
+/**
+ * Test configuration for the FSE plugin.
+ *
+ * Will match files such that:
+ *   1. Must be in the apps/full-site-editing/ directory
+ *   2. Must have .test.EXT or .spec.EXT at the end of the filename
+ *   3. EXT (above) must be one of js, ts, jsx, or tsx.
+ */
+
+/* eslint-disable import/no-extraneous-dependencies */
+
 // @wordpress/scripts manually adds additional Jest config ontop of
 // @wordpress/jest-preset-default so we pull in this file to extend it
 const defaults = require( '@wordpress/scripts/config/jest-unit.config.js' );
+const path = require( 'path' );
 
-module.exports = {
+const config = {
 	...defaults,
+	rootDir: path.normalize( '../../' ), // To detect wpp-calypso root node_modules
+	testMatch: [ `${ __dirname }/**/?(*.)+(spec|test).[jt]s?(x)` ],
 	setupFilesAfterEnv: [
 		...( defaults.setupFilesAfterEnv || [] ), // extend if present
-		'<rootDir>/test/client/test-setup',
+		'<rootDir>/apps/full-site-editing/bin/js-unit-setup',
 	],
 };
+
+module.exports = config;
