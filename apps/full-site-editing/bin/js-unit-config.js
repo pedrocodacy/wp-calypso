8dfa998e182e68e4c4b7ccecd9b9f6c11c8586eb
Noah Allen <noahtallen@gmail.com>
2020-01-24 15:11:13 -0800

    Fix various things with e2e/unit tests
    - e2e tests now use default config
    - jsunit tests only test unit files

--- apps/full-site-editing/bin/js-unit-config.js
+++ apps/full-site-editing/bin/js-unit-config.js
@@ -3,23 +3,29 @@
  *
  * Will match files such that:
  *   1. Must be in the apps/full-site-editing/ directory
- *   2. Must have .test.EXT or .spec.EXT at the end of the filename
+ *   2. Must have .test.EXT at the end of the filename
  *   3. EXT (above) must be one of js, ts, jsx, or tsx.
+ * 
+ * Note: In order to use a different jest conig for e2e tests, this config file
+ * must be kept in the bin/ folder to prevent it from being detected as the
+ * config file for e2e tests.
  */
 
 /* eslint-disable import/no-extraneous-dependencies */
-
-const suffix = process.argv.includes( 'e2e' ) ? 'spec' : 'test';
 
 // @wordpress/scripts manually adds additional Jest config ontop of
 // @wordpress/jest-preset-default so we pull in this file to extend it
 const defaults = require( '@wordpress/scripts/config/jest-unit.config.js' );
 const path = require( 'path' );
 
+// Basically, CWD, so 'apps/full-site-editing'.
+// Without this, it tries to use 'apps/full-site-editing/bin'
+const pluginRoot = path.resolve( './' );
+
 const config = {
 	...defaults,
-	rootDir: path.normalize( '../../' ), // To detect wpp-calypso root node_modules
-	testMatch: [ `${ __dirname }/**/?(*.)${ suffix }.[jt]s?(x)` ],
+	rootDir: path.normalize( '../../../' ), // To detect wpp-calypso root node_modules
+	testMatch: [ `${ pluginRoot }/**/?(*.)test.[jt]s?(x)` ],
 	setupFilesAfterEnv: [
 		...( defaults.setupFilesAfterEnv || [] ), // extend if present
 		'<rootDir>/apps/full-site-editing/bin/js-unit-setup',
