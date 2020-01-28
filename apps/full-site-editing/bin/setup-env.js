4673a285c5dcb85c9cac7712282ef029dd98cb9f
Noah Allen <noahtallen@gmail.com>
2020-01-28 13:57:24 -1000

    Theoretically run npm ci before setting up the dev environment

--- apps/full-site-editing/bin/setup-env.js
+++ apps/full-site-editing/bin/setup-env.js
@@ -4,6 +4,18 @@
 /* eslint-disable import/no-nodejs-modules */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
+const { execSync } = require('child_process');
+
+const fseDir = process.cwd();
+// Change to wp-calypso root:
+process.chdir( '../../' );
+
+console.log( process.cwd() );
+
+execSync( 'npm ci' );
+
+process.chdir( fseDir );
+// Require it after running the install script
 const runAll = require( 'npm-run-all' );
 
 runAll( [ 'build', 'env install' ], {
